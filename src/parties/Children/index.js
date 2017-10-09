4ea255d38ff22e0a811283997724860103e681d7
Bipin Patel <32482638+bipin-tms@users.noreply.github.com>
2017-10-09 15:03:31 +0530

    Added phone number class for parties-AMAAS-1707

--- src/parties/Children/index.js
+++ src/parties/Children/index.js
@@ -1,3 +1,3 @@
 export { default as Address } from './address.js'
 export { default as Email } from './email.js'
-export { default as PhoneNumber } from './phoneNumber.js
+export { default as PhoneNumber } from './phoneNumber.js'
