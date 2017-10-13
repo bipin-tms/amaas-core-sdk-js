cea6d0fa66817a70e8b57d5feb5592cd5261e362
Bipin Patel <32482638+bipin-tms@users.noreply.github.com>
2017-10-13 12:00:31 +0530

    Update exchange.js

--- src/parties/Exchange/exchange.js
+++ src/parties/Exchange/exchange.js
@@ -21,6 +21,7 @@ class Exchange extends Company {
    * @param {string} [params.yearOfIncorporation] - Year of incorporation
    * @param {object} [params.addresses] - Object of Addresses associated with the Exchange
    * @param {object} [params.emails] - Object of Emails associated with the Exchange
+   * @param {object} [params.phoneNumbers] - Object of phoneNumbers associated with the Exchange
    * @param {object} [params.references] - Object of References associated with the Exchange
    * @param {object} [params.comments] - Object of Comments associated with the Exchange
    * @param {object} [params.links] - Object of Links associated with the Exchange
@@ -42,6 +43,7 @@ class Exchange extends Company {
     yearOfIncorporation,
     addresses={},
     emails={},
+    phoneNumbers={},
     references={},
     comments={},
     links={},
@@ -63,6 +65,7 @@ class Exchange extends Company {
       yearOfIncorporation,
       addresses,
       emails,
+      phoneNumbers,
       references,
       comments,
       links,
